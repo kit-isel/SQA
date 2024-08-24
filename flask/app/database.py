de4fdb94e9b2f3d16153951ca24681b97c32c584
miyanagas <zs0h8po2t7ad@gmail.com>
2024-08-24 13:02:48 +0900

    ✨  データベースの設定と初期化を追加

---
+++ flask/app/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+password = os.environ.get("MYSQL_ROOT_PASSWORD")
+dbname = os.environ.get("MYSQL_DATABASE")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:{password}@mysql:3306/{dbname}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                            autoflush=False,
+                                            bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    import app.models
+    Base.metadata.create_all(bind=engine)
