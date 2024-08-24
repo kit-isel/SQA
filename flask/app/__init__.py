02991d56bbfe326fa047300e58cb2de0e26a960d
miyanagas <zs0h8po2t7ad@gmail.com>
2024-08-24 13:01:44 +0900

    ✨ Flaskアプリの初期設定を追加
    DBの初期化、セッション削除の設定追加とBlueprintの登録

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,31 +1,26 @@
 import os
-
 from flask import Flask
 
-def create_app(test_config=None):
+from app.database import init_db
+from app.database import db_session
+from app import routes
+
+def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
-    )
+    app.config.from_pyfile('config.py', silent=True)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    init_db()
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/')
+    def hello_world():
+        return "Hello World"
+
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db_session.remove()
+
+    app.register_blueprint(routes.bp)
 
     return app
