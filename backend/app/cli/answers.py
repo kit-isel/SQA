2e151901e2eaf9097d48754d54f7a60ef707aae3
ry05k2ulv <ryosuke.10117@gmail.com>
2024-09-24 12:04:19 +0900

    ✨feat: Questions,Answersのデータを追加するCLIコマンドを追加 #18

--- backend/app/cli/answers.py
+++ backend/app/cli/answers.py
@@ -15,3 +15,14 @@ bp = Blueprint("answers", __name__)
 def read_answers(sort: str):
     answers = crud.read_answers(SortType.from_str(sort))
     pp([answer.to_dict() for answer in answers])
+
+
+@bp.cli.command("create")
+@click.argument("question_id")
+@click.argument("description")
+def create_answer(question_id: int, description: str):
+    if crud.read_question_by_id(question_id):
+        answer = crud.create_answer(question_id, description)
+        pp(answer.to_dict())
+    else:
+        print("error", "question not found")
--- backend/app/cli/qustions.py
+++ backend/app/cli/qustions.py
@@ -14,4 +14,12 @@ bp = Blueprint("questions", __name__)
 )
 def read_questions(sort: str):
     questions = crud.read_questions(SortType.from_str(sort))
-    pp([question.to_limited_dict() for question in questions])
+    pp([question.to_dict() for question in questions])
+
+
+@bp.cli.command("create")
+@click.argument("title")
+@click.argument("description")
+def create_question(title: str, description: str):
+    question = crud.create_question(title, description)
+    pp(question.to_dict())
